051d3700918afa6511d056489f3a44b4c0223116
Nikiforos88 <nikiforosamg@gmail.com>
2024-04-28 13:55:05 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CalendarComponent, TaskListComponent],
+  imports: [RouterOutlet, NavbarComponent, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatCardModule, MatDatepickerModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+
+
   niki() {
     console.log('works');
+    console.log(this.selected);
+  }
+
+  selected!: Date | null;
+
+  onDateSelected(date: Date) {
+    if (this.selected !== null) {
+      this.selected = date;
+    }
   }
 }
